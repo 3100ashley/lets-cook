bb96231e9521517ac6defcb97ce39a1d00057edf
Ashley Manese <ashleymanese31@gmail.com>
2023-08-29 18:16:49 -0700

    feat: a button to add indv recipe

---
+++ src/components/AddRecipe.js
@@ -0,0 +1,154 @@
+import { useState } from "react";
+import Modal from "react-bootstrap/Modal";
+
+export default function AddRecipe(props) {
+  const [name, setName] = useState("");
+  const [instructions, setInstructions] = useState("");
+  const [ingredients, setIngredients] = useState("");
+  const [image, setImage] = useState("")
+  const [show, setShow] = useState(props.show);
+  const handleClose = () => setShow(false);
+ 
+
+  return (
+    <>
+      <button
+        onClick={props.toggleShow}
+        className="block m-2 bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 px-3 rounded"
+      >
+        + Add Recipe
+      </button>
+      <Modal
+        show={props.show}
+        onHide={handleClose}
+        backdrop="static"
+        keyboard={false}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Add Recipe</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <form
+            onSubmit={(e) => {
+              props.toggleShow();
+              e.preventDefault();
+              props.newRecipe(name, ingredients, instructions, image)
+              setName('')
+              setIngredients('')
+              setInstructions('')
+              setImage('')
+
+            }}
+            id="editmodal"
+            className="w-full max-w-sm"
+          >
+            <div className="md:flex md:items-center mb-6">
+              <div className="md:w-1/3">
+                <label
+                  className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                  for="name"
+                >
+                  Name
+                </label>
+              </div>
+              <div className="md:w-2/3">
+                <input
+                  className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                  id="name"
+                  type="text"
+                  placeholder="Bread"
+                  value={name}
+                  onChange={(e) => {
+                    setName(e.target.value);
+                  }}
+                />
+              </div>
+            </div>
+            <div className="md:flex md:items-center mb-6">
+              <div className="md:w-1/3">
+                <label
+                  className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                  for="ingredients"
+                >
+                  Ingredients
+                </label>
+              </div>
+              <div className="md:w-2/3">
+                <input
+                  className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                  id="ingredients"
+                  type="text"
+                  placeholder="flour,eggs,milk"
+                  value={ingredients}
+                  onChange={(e) => {
+                    let ingredientsInput = e.target.value;
+                    setIngredients(ingredientsInput.split(','));
+                  }}
+                />
+              </div>
+            </div>
+            <div className="md:flex md:items-center mb-6">
+              <div className="md:w-1/3">
+                <label
+                  className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                  for="instructions"
+                >
+                  Instructions
+                </label>
+              </div>
+              <div className="md:w-2/3">
+                <input
+                  className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                  id="instructions"
+                  type="text"
+                  placeholder="Place in oven"
+                  value={instructions}
+                  onChange={(e) => {
+                    setInstructions(e.target.value);
+                  }}
+                />
+              </div>
+            </div>
+            <div className="md:flex md:items-center mb-6">
+              <div className="md:w-1/3">
+                <label
+                  className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                  for="image"
+                >
+                  Image
+                </label>
+              </div>
+              <div className="md:w-2/3">
+                <input
+                  className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                  id="image"
+                  type="text"
+                  placeholder="https://www.pexels.com/photo/sliced-bread-on-gray-surface-1775043/"
+                  value={image}
+                  onChange={(e) => {
+                    setImage(e.target.value);
+                  }}
+                />
+              </div>
+            </div>
+          </form>
+        </Modal.Body>
+        <Modal.Footer>
+          <button
+            className="bg-slate-400 hover:bg-slate-500 text-white font-bold py-2 px-3 rounded"
+            onClick={props.toggleShow}
+          >
+            Close
+          </button>
+          <button
+            className="bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 px-3 rounded"
+            form="editmodal"
+          >
+            Add
+          </button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
+
--- src/pages/CreatedRecipes.js
+++ src/pages/CreatedRecipes.js
@@ -2,8 +2,16 @@ import { useEffect, useState } from "react";
 import RecipeCard from "../components/RecipeCard";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
+import randomInteger from "random-int";
+import AddRecipe from "../components/AddRecipe";
 export default function MyRecipes() {
   const [recipes, setRecipes] = useState();
+  const [show, setShow] = useState(false);
+
+  function toggleShow() {
+    setShow(!show);
+  }
+
   useEffect(() => {
     async function fetchData() {
       try {
@@ -16,21 +24,59 @@ export default function MyRecipes() {
     }
 
     fetchData();
-  }, []);
+  }, [recipes]);
 
+
+  function newRecipe(name, ingredients, instructions, image) {
+    let number = randomInteger(500);
+    const data = {
+      id: number,
+      name: name,
+      ingredients: ingredients,
+      instructions: instructions,
+      image: image,
+    };
+    fetch("http://localhost:8000", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+
+      body: JSON.stringify(data),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Something went wrong");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        toggleShow();
+        console.log(data)
+        setRecipes([...recipes, data.recipe]);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }
 
   return (
     <>
-      <h1 className="text-center">Created Recipes</h1>
+      <div className="flex justify-center flex-col">
+        <h1 className="text-center">Created Recipes</h1>
+        <AddRecipe newRecipe={newRecipe} show={show} toggleShow={toggleShow} />
+      </div>
       <div className="flex flex-wrap gap-y-10 justify-evenly mx-10">
         {recipes &&
           recipes.map((recipe) => {
-            return <RecipeCard width="w-72" img={recipe.image}>
+            return (
+              <RecipeCard width="w-72" img={recipe.image}>
                 <Card.Title>{recipe.name}</Card.Title>
                 <Button variant="dark" href={"/createdrecipe/" + recipe.id}>
                   Go to recipe
                 </Button>
-            </RecipeCard>;
+              </RecipeCard>
+            );
           })}
       </div>
     </>
